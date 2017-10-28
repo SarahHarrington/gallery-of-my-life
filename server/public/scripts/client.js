99eda9482d0315a7badfb65dbb1e4b80fbc1e3b5
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-10-28 14:17:12 -0500

    indivudal picture clicks hiding picture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,35 +1,47 @@
 var myApp = angular.module('myApp', []);
 
+function pic(u, d) {
+    return {
+        image: u,
+        description: d, 
+        showPic: true,
+        showDesc: true,
+        heartCounter: 0,
+        viewCounter: 0,
+        comments: []
+    }
+}
+
 myApp.controller('galleryController', function($http){
     var gallery = this;
     var imageLikes = 0;
+    var id= 0;
 
     var pictures = [
-        {image: 'https://i.imgur.com/eGMLOdN.jpg', description: 'The fancy bike.'},
-        {image: 'https://i.imgur.com/T05RHQ0.jpg', description: 'The baking.'},
-        {image: 'https://i.imgur.com/AoxShhG.jpg', description: 'The husband.'},
-        {image: 'https://i.imgur.com/zeCVdgH.jpg', description: 'The next step.'},
-        {image: 'https://i.imgur.com/H2MYrM5.jpg', description: 'The kids.'},
-        {image: 'https://i.imgur.com/kmu0BOv.jpg', description: 'The trouble.'},
-        {image: 'https://i.imgur.com/gJFGrXT.jpg', description: 'The goofball.'},
-        {image: 'https://i.imgur.com/0YvtuT6.jpg', description: 'The cat.'},
-        {image: 'https://i.imgur.com/1J6rXpv.jpg', description: 'The yard.'},
-        {image: 'https://i.imgur.com/dts31g1.jpg', description: 'The quotes.'},
-        {image: 'https://i.imgur.com/CcNmNn7.jpg', description: 'The family time.'}
+        pic('https://i.imgur.com/eGMLOdN.jpg', 'The fancy bike.'),
+        pic('https://i.imgur.com/T05RHQ0.jpg', 'The baking.'),
+        pic('https://i.imgur.com/AoxShhG.jpg', 'The husband.'),
+        pic('https://i.imgur.com/zeCVdgH.jpg', 'The next step.'),
+        pic('https://i.imgur.com/H2MYrM5.jpg', 'The kids.'),
+        pic('https://i.imgur.com/kmu0BOv.jpg', 'The trouble.'),
+        pic('https://i.imgur.com/gJFGrXT.jpg', 'The goofball.'),
+        pic('https://i.imgur.com/0YvtuT6.jpg', 'The cat.'),
+        pic('https://i.imgur.com/1J6rXpv.jpg', 'The yard.'),
+        pic('https://i.imgur.com/dts31g1.jpg', 'The quotes.'),
+        pic('https://i.imgur.com/CcNmNn7.jpg', 'The family time.')
     ];
 
     gallery.pictures = pictures;
     gallery.descriptionHidden = true;
 
-    gallery.imageClicked = function(picture) {
-        console.log('image clicked');
-        gallery.descriptionHidden = false;
-    } 
+    gallery.showCard = function(i) {
+        console.log('image clicked', i);
+        pictures[i].showPic = false;
+        pictures[i].showDisc = false;
+    };
 
-    gallery.description = function(description) {
-        console.log('description clicked');
-        
-    }
+
+
 
 
 }) //end of gallery controller

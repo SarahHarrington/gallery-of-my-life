a42e5f58731309b9086d6bdd2661cfb290844928
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-10-29 21:14:14 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var port = 5000;
+var port = process.env.PORT || 5000;;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
